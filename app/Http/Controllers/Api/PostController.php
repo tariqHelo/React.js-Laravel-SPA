78cbb642aa0f5dcc5ef406e88da4b6aa4f3856df
unknown <tariq.1994t12@gmail.com>
2022-06-10 21:13:53 +0300

    AxiosAPI&GetPosts

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PostResource::collection(Post::Paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/js/Pages/Posts/index.js
+++ resources/js/Pages/Posts/index.js
@@ -1,36 +1,89 @@
+import { component, useEffect, useState } from 'react';
+function PostIndex() {
 
+    const [posts, setPosts] = useState([]);
 
-export default function PostIndex() {
+    async function fetchPosts() {
+        const response = await fetch('http://127.0.0.1:8000/api/posts');
+        const data = await response.json();
+        console.log(data);
+        setPosts(data.data);
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
-        // <div>
-        //     <h1>Posts will be here......</h1>
-        // </div>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Content</th>
-                    <th>Create At</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Post 1</td>
-                    <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                        containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                        including versions of Lorem Ipsum.
-                    </td>
-                    <td>2020-01-01</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+            <div className="min-w-full align-middle">
+                <table className="table">
+                    <thead className="table-header">
+                        <tr>
+                            <th>
+                                <span>ID</span>
+                            </th>
+                            <th>
+                                <span>Title</span>
+                            </th>
+                            <th>
+                                <span>Content</span>
+                            </th>
+                            <th>
+                                <span>Created at</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-body">
+                        {posts.map(post => {
+                            return (
+                                <tr key={post.id}>
+                                    <td>{post.id}</td>
+                                    <td>{post.title}</td>
+                                    <td>{post.content}</td>
+                                    <td>{post.created_at}</td>
+                                </tr>
+                            );
+                        })}
 
-    )
+                        {/* <tr>
+                            <td>1</td>
+                            <td>A</td>
+                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                            <td>2022-01-01 13:43:47</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>B</td>
+                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                            <td>2022-01-02 14:43:47</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>C</td>
+                            <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+                            <td>2022-01-03 15:43:47</td>
+                        </tr> */}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 
 }
+export default PostIndex;
